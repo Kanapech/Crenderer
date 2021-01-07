0996a14f68ae891de4dc5c23238534d7b90c21dc
Kanapech <silver36tv@gmail.com>
2021-01-07 13:43:56 +0100

    Affichage nuage de points

--- Crenderer.cpp
+++ Crenderer.cpp
@@ -1,10 +1,13 @@
 #include <cmath>
 #include <iostream>
 #include <algorithm>
+#include <fstream>
+#include <vector>
 #include "tgaimage.h"
+using namespace std;
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
-const TGAColor red   = TGAColor(255, 0,   0,   255);
+const TGAColor red   = TGAColor(0 , 0,   255,   255);
 
 void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
     bool steep = false;
@@ -38,13 +41,30 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
 }
 
 int main(int argc, char** argv) {
-    TGAImage image(100, 100, TGAImage::RGB);
-    for (int i=0; i<1000000; i++) {
-        line(13, 20, 80, 40, image, white);
-        line(20, 13, 40, 80, image, red);
-        line(80, 40, 13, 20, image, red);
+    string v, ligne;
+    //float vert[2][1258];
+    float x, y, z; 
+    //int count = 0;
+    TGAImage image(200, 200, TGAImage::RGB);
+    
+    ifstream file("african_head.obj");
+	if (!file) 
+		cout << "Fichier non trouvé";
+	
+    while (getline(file, ligne)) {
+        if(ligne.rfind("v ", 0) == 0){
+            file >> v >>  x >> y >> z;
+            //vert[0][count] = x ; vert[1][count] = z;
+            //cout << vert[0][count] << vert[1][count] << endl;
+            //count++;
+            
+            line(x*100+100, y*100+100, x*100+100, y*100+100, image, white);     
+        }
     }
+
     image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
     image.write_tga_file("output.tga");
+
+    file.close(); 
     return 0;
 }
