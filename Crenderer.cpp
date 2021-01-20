60712a41bf7dd70372d8342220b8a29d07506a6b
Kanapech <silver36tv@gmail.com>
2021-01-20 20:24:56 +0100

    Traçage triangles

--- Crenderer.cpp
+++ Crenderer.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <vector>
 #include "tgaimage.h"
+#include <sstream>
 using namespace std;
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
@@ -42,25 +43,45 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
 
 int main(int argc, char** argv) {
     string v, ligne;
-    //float vert[2][1258];
+    float vert[3][1258];
+    int faces[3][2492];
     float x, y, z; 
-    //int count = 0;
+    int vcount = 0;
+    int fcount = 0;
+    string a, b, c;
     TGAImage image(200, 200, TGAImage::RGB);
     
     ifstream file("african_head.obj");
-	if (!file) 
-		cout << "Fichier non trouvé";
-	
-    while (getline(file, ligne)) {
+	if (!file){
+        cout << "Fichier non trouvé";
+        exit(EXIT_FAILURE);
+    }
+    while (getline(file, ligne)) {   
+        std::istringstream iss(ligne.c_str());
         if(ligne.rfind("v ", 0) == 0){
-            file >> v >>  x >> y >> z;
-            //vert[0][count] = x ; vert[1][count] = z;
-            //cout << vert[0][count] << vert[1][count] << endl;
-            //count++;
+            iss >> v >> x >> y >> z;
+            vert[0][vcount] = x ; vert[1][vcount] = y; vert[2][vcount] = z;
+            //cout << vert[0][vcount] << vert[1][vcount] << vert[2][vcount] << endl;
+            vcount++;
+            //line(x*100+100, y*100+100, x*100+100, y*100+100, image, white);
             
-            line(x*100+100, y*100+100, x*100+100, y*100+100, image, white);     
+        }
+        if(ligne.rfind("f ", 0) == 0){
+            iss >> v >> a >> b >> c;
+            a = a.substr(0, a.find("/"));
+            b = b.substr(0, b.find("/"));
+            c = c.substr(0, c.find("/"));
+            faces[0][fcount] = stoi(a) ; faces[1][fcount] = stoi(b); faces[2][fcount] = stoi(c);
+            //cout << faces[0][fcount] << faces[1][fcount] << faces[2][fcount] << endl;
+            fcount++;
         }
     }
+
+    for(int i = 0; i<fcount; i++){
+        line(vert[0][faces[0][i]]*100+100, vert[1][faces[0][i]]*100+100, vert[0][faces[1][i]]*100+100, vert[1][faces[1][i]]*100+100, image, white);
+        line(vert[0][faces[1][i]]*100+100, vert[1][faces[1][i]]*100+100, vert[0][faces[2][i]]*100+100, vert[1][faces[2][i]]*100+100, image, white);
+        line(vert[0][faces[2][i]]*100+100, vert[1][faces[2][i]]*100+100, vert[0][faces[0][i]]*100+100, vert[1][faces[0][i]]*100+100, image, white);
+    } 
 
     image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
     image.write_tga_file("output.tga");
